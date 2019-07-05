29fd03e8a1e96a36e8a67ebf79a16ab6e54446e5
Ritik Srivastava <ritiksrivastava025@gmail.com>
2019-07-05 19:25:03 +0530

    Added authcheck

--- routes/contests.js
+++ routes/contests.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+//load authcheck
+const { isLoggedIn } = require('../config/authcheck');
+
 //load controllers
 const { index } = require('../controllers/contests_controller');
 
 //ongoing and upcoming contests
-router.get('/', index);
+router.get('/',isLoggedIn, index);
 
 //export router
 module.exports = router;
